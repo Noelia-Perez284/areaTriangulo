9b16ca734a0aa6f3e7b78a758134ecc48a575d72
Noelia-Perez284 <102192324+Noelia-Perez284@users.noreply.github.com>
2022-04-21 13:11:16 -0300

    area_triangulo Consigna

--- src/areaTriangulo.ts
+++ src/areaTriangulo.ts
@@ -1,3 +1,10 @@
+//Realice un programa que devuelva el área del
+// triangulo usando los siguientes pares de base-altura:
+// • (1,2) (2,4) (3,6) (4,8) (5, 10) (6,12) (7,14)
+// • Implemente un método llamado calcularAreaTriangulo
+// que reciba dos números por parámetro (uno llamado
+// base y otro altura)
+
 let btnEnviar = <HTMLButtonElement>document.getElementById("btnEnviar");
 let inputBase = <HTMLInputElement>document.getElementById("base");
 let inputAltura = <HTMLInputElement>document.getElementById("altura");
